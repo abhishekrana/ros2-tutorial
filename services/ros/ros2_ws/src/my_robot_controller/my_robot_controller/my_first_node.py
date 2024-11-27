4c449cc1b87aa8a97f0728dd3e7ec8faaf487e6e
Abhishek Rana <abhishek.rana@agile-robots.com>
2024-11-27 13:20:36 +0100

    feat: Implement MyNode class in my_first_node.py for basic ROS2 functionality

--- services/ros/ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ services/ros/ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 import rclpy
+from rclpy.node import Node
+
+
+class MyNode(Node):
+    def __init__(self) -> None:
+        super().__init__("first_node")
+        self.get_logger().info("Hello from ROS2")
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+
+    # Can also create multiple nodes from the same program
+    node = MyNode()
+
+    # Keep node running until stopped (e.g. Ctrl+C). Returns when Ctrl+C is pressed.
+    rclpy.spin(node)
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
