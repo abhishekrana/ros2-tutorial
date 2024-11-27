a06634d41e9ce912de1a3fd00930a6d892ff4383
Abhishek Rana <abhishek.rana@agile-robots.com>
2024-11-27 15:17:50 +0100

    feat: Add Turtle Controller node and update Taskfile for new command

---
+++ services/ros/ros2_ws/src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+
+class TurtleControllerNode(Node):
+    def __init__(self) -> None:
+        super().__init__("pose_subscriber")  # Can be different from the file name
+        self.pose_subscriber = self.create_subscription(
+            Pose, "/turtle1/pose", self.pose_callback, 10
+        )
+        self.cmd_vel_pub = self.create_publisher(
+            Twist, "/turtle1/cmd_vel", 10
+        )  # 10 is the queue size
+        self.get_logger().info("Turtle Controller Node has been started.")
+
+    def pose_callback(self, msg: Pose) -> None:
+        self.get_logger().info(
+            f"Received pose: x={msg.x}, y={msg.y}, theta={msg.theta}"
+        )
+
+        if msg.x > 8:
+            msg = Twist()
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+            self.cmd_vel_pub.publish(msg)
+            self.get_logger().info("Reached the destination.")
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+    node = TurtleControllerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
